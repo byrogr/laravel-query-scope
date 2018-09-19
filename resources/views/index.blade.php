8d2d8568c4c174ac1170b0f09be4776ae9993343
Roger Rojas <roger.rojas@setours.com>
2018-09-19 10:44:47 -0500

    agregando bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,48 +2,70 @@
 <html>
 <head>
     <title>Busqueda</title>
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
 </head>
 <body>
-    <h1>Busqueda</h1>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <h1>Busqueda</h1>
+            </div>
+            <div class="col-md-8">
+                <form action="{{ route('buscar') }}"
+                      method="get"
+                      class="form-inline">
+                    <input type="text" name="name" class="form-control mr-2" placeholder="Nombre...">
+                    <input type="text" name="email" class="form-control mr-2" placeholder="Email...">
+                    <input type="text" name="bio" class="form-control mr-2" placeholder="Bio..">
+                    <button type="submit" class="btn btn-primary">Buscar</button>
+                </form>
+            </div>
+        </div>
 
-    <form action="{{ route('buscar') }}" method="get">
-        <div>
-            <label>Nombre:</label>
-            <input type="text" name="name">
+        <div class="row">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-borderer">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>Bio</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($usuarios as $u)
+                            <tr>
+                                <td>{{ $u->id }}</td>
+                                <td>{{ $u->name }}</td>
+                                <td>{{ $u->email }}</td>
+                                <td>{{ $u->bio }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer">
+                    {{ $usuarios->render() }}
+                </div>
+            </div>
         </div>
-        <div>
-            <label>Email</label>
-            <input type="text" name="email">
-        </div>
-        <div>
-            <label>Bio</label>
-            <input type="text" name="bio">
-        </div>
-        <div>
-            <button type="submit">Buscar</button>
-        </div>
-    </form>
-    <hr>
-    <table border="1" width="100%">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nombre</th>
-                <th>Email</th>
-                <th>Bio</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($usuarios as $u)
-                <tr>
-                    <td>{{ $u->id }}</td>
-                    <td>{{ $u->name }}</td>
-                    <td>{{ $u->email }}</td>
-                    <td>{{ $u->bio }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $usuarios->render() }}
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
 </body>
 </html>
